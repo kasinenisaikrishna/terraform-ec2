747ce01a5ac9a642bd1268d211246624d787faae
kasinenisaikrishna <kasineni.sai@gmail.com>
2025-09-30 17:21:53 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_names" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  default = "Z033712422B61CO3J7OXS"
+}
+
+variable "domain_name" {
+  default = "dawsconnect.org"
+}
